502405716266459ae02e48b645b613b46e9bbc92
bassamadnan <bassamadnan2012@gmail.com>
2025-06-10 16:58:59 +0530

    try again

--- test/testsuite.sh
+++ test/testsuite.sh
@@ -431,7 +431,7 @@ execute_testcases() {
       exit 1
    fi
    ### RUN TESTCASES ###
-   $TEST_DIRECTORY/pgexporter_test
+   $TEST_DIRECTORY/pgexporter_test $PROJECT_DIRECTORY $PROJECT_DIRECTORY
    if [ $? -ne 0 ]; then
       # Kill pgexporter
       pkill -f pgexporter || true
