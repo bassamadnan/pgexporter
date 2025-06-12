c33ee80d5e57072c2c7295ac8d4ac19c0bf33a1f
bassamadnan <bassamadnan2012@gmail.com>
2025-06-12 13:25:48 +0530

    swap line

--- test/testcases/pgexporter_test_3.c
+++ test/testcases/pgexporter_test_3.c
@@ -74,8 +74,8 @@ pgexporter_test3_suite()
    tc_core = tcase_create("Core");
 
    tcase_set_timeout(tc_core, 60);
-   tcase_add_test(tc_core, test_pgexporter_http_metrics);
    tcase_add_test(tc_core, test_pgexporter_bridge_endpoint);
+   tcase_add_test(tc_core, test_pgexporter_http_metrics);
    tcase_add_test(tc_core, test_pgexporter_extension_detection);
 
    tcase_add_test(tc_core, test_pgexporter_shutdown);
